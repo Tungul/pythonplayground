80e53ca1981ebaf45685f92e0f014b9123793f73
tungul <lugnut1206@gmail.com>
2013-09-24 22:15:57 -0700

    moved to another file

--- android/chatwebview.py
+++ android/chatwebview.py
@@ -1,26 +1,5 @@
 import android
-
-#  switch library. can I put this in a .py and run import switch?
-# not mine
-class switch(object):
-    def __init__(self, value):
-        self.value = value
-        self.fall = False
-
-    def __iter__(self):
-        """Return the match method once, then stop"""
-        yield self.match
-        raise StopIteration
-    
-    def match(self, *args):
-        """Indicate whether or not to enter a case suite"""
-        if self.fall or not args:
-            return True
-        elif self.value in args: # changed for v1.5, see below
-            self.fall = True
-            return True
-        else:
-            return False
+import switch
 
 droid = android.Android()
 droid.webViewShow('file:///sdcard/sl4a/scripts/text_to_speech.html')
