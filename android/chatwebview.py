142ec48a0d6208e2edc592277d3fdb365cab5e8b
tungul <lugnut1206@gmail.com>
2013-10-30 00:01:05 -0700

    more work from a really long time ago

--- android/chatwebview.py
+++ android/chatwebview.py
@@ -1,10 +1,19 @@
 import android
-import switch
+# import switch
+import socket
+
+c = socket.create_connection(("192.168.1.148", 8675))
+c.send("connected...\n")
 
 droid = android.Android()
-droid.webViewShow('file:///sdcard/sl4a/scripts/text_to_speech.html')
+droid.webViewShow('file:///sdcard/sl4a/scripts/chatwebview.html')
 while True:
-	map = droid.eventWait()
+	# data = c.recv(1024)
+	# print data.replace("\n", "")
+	# droid.eventPost("berp", data)
+
+	map = droid.eventWaitFor("connect").result
+	droid.makeToast("got event")
 	print map
 	# for case in switch()
 	# droid.ttsSpeak(result['data'])
