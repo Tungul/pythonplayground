82fd40a2aac361d26c97d726ede8b451fe912f6e
Tungul <lugnut1206@gmail.com>
2013-10-31 23:39:13 -0700

    completed

--- sieve.py
+++ sieve.py
@@ -1,17 +1,26 @@
-import threading
+import threading, androidhelper
+
+droid = androidhelper.Android()
+
+target = 1000000
 
 toWrite = []
 output = open('/storage/emulated/0/primes.txt', 'w')
 
-class WriteOutPrimes(threading.Thread)
-	def __init__(self)
+class WriteOutPrimes(threading.Thread):
+	def __init__(self):
 		threading.Thread.__init__(self)
-	def run(self)
+	def run(self):
 		while True:
-			if toWrite[0] != None:
-				output.write(str(toWrite.pop(0)) + '\n')
-			elif toWrite[0] == 'Done'
-				break
+			try:
+				value = toWrite.pop(0)
+				if value != None and value != 'Done':
+					output.write(str(value) + '\n')
+#					print value
+				elif value == 'Done':
+					break
+			except:
+				continue
 
 source = []
 primes = []
@@ -19,15 +28,18 @@ primes = []
 outputThread = WriteOutPrimes()
 outputThread.start()
 
-for i in range(1, 5000):
+for i in range(1, target):
 	source.append(1)
 
-for i in range(2, len(source)):
+for i in range(2, target):
 	if source[i] == 1:
 		toWrite.append(i)
-		for j in range(i+i, len(source), i):
+		for j in range(i+i, target, i):
 			source[j] = 0
+	if i == (target / 2):
+		droid.makeToast('Halfway there.')
 
+toWrite.append('Done')
 outputThread.join()
 
-f.close()
+output.close()
